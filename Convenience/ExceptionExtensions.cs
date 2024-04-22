d73810e9aeea437595e0bb81c31b4bab001a31cb
andrerodi <89607119+andrerodi@users.noreply.github.com>
2024-04-22 22:01:06 +0200

    Added extension method FlattenExceptionMessages

---
+++ Convenience/ExceptionExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace Extensions;
+
+public static class ExceptionExtensions
+{
+    /// <summary>
+    /// Returns a flattened array of all exception messages, including inner exceptions.
+    /// </summary>
+    /// <param name="ex"></param>
+    /// <returns></returns>
+    public static string[] FlattenExceptionMessages(this Exception? ex)
+        => ex.FlattenExceptionMessagesIterator().ToArray();
+
+    /// <summary>
+    /// The recursive iterator that flattens the exception messages.
+    /// </summary>
+    /// <param name="ex"></param>
+    /// <returns></returns>
+    private static IEnumerable<string> FlattenExceptionMessagesIterator(this Exception? ex)
+    {
+        if (ex is null || string.IsNullOrWhiteSpace(ex.Message))
+        {
+            yield break;
+        }
+
+        yield return ex.Message;
+
+        foreach (var message in FlattenExceptionMessages(ex.InnerException))
+        {
+            yield return message;
+        }
+    }
+}
